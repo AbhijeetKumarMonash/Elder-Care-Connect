c36b5f4198500301140834472c5583af8b080c54
AbhijeetKumarMonash <141550630+AbhijeetKumarMonash@users.noreply.github.com>
2024-09-03 17:09:45 +1000

    Login PAge modified and made responsive

--- iad-project/src/views/LoginView.vue
+++ iad-project/src/views/LoginView.vue
@@ -1,12 +1,132 @@
 <template>
-  <div class="Login">
-    <h1>Welcome To login</h1>
-    <p>Under configuration.</p>
+  <div class="container mt-5">
+    <BHeader />
+    <div class="row justify-content-center">
+      <div class="col-xl-4 col-lg-6 col-md-8 col-sm-10">
+        <h1 class="text-center">Elder Care Connect</h1>
+        <h3 class="text-center">Welcome To login</h3>
+        <form @submit.prevent="submitForm">
+          <div class="row mb-3 justify-content-center">
+            <div class="col-12">
+              <label for="username" class="form-label">Username</label>
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                @blur="() => validateName(true)"
+                @input="() => validateName(false)"
+                v-model="formData.username"
+              />
+              <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
+            </div>
+          </div>
+          <div class="row mb-3 justify-content-center">
+            <div class="col-12">
+              <label for="password" class="form-label">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                @blur="() => validatePassword(true)"
+                @input="() => validatePassword(false)"
+                v-model="formData.password"
+              />
+              <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
+            </div>
+          </div>
+          <div class="text-center">
+            <button type="submit" class="btn btn-primary me-2">Submit</button>
+            <button type="button" class="btn btn-secondary" @click="clearForm">Clear</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-// No script needed for now
+import BHeader from '@/components/BHeader.vue'
+import { ref } from 'vue'
+
+const formData = ref({
+  username: '',
+  password: ''
+})
+
+const submittedCards = ref([])
+
+const submitForm = () => {
+  validateName(true)
+  validatePassword(true)
+  if (!errors.value.username && !errors.value.password) {
+    submittedCards.value.push({ ...formData.value })
+    clearForm()
+  }
+}
+
+const clearForm = () => {
+  formData.value = {
+    username: '',
+    password: ''
+  }
+}
+const errors = ref({
+  username: null,
+  password: null
+})
+
+const validateName = (blur) => {
+  if (formData.value.username.length < 3) {
+    if (blur) errors.value.username = 'Name must be at least 3 characters'
+  } else {
+    errors.value.username = null
+  }
+}
+
+const validatePassword = (blur) => {
+  const password = formData.value.password
+  const minLength = 8
+  const hasUppercase = /[A-Z]/.test(password)
+  const hasLowercase = /[a-z]/.test(password)
+  const hasNumber = /\d/.test(password)
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password)
+
+  if (password.length < minLength) {
+    if (blur) errors.value.password = `Password must be at least ${minLength} characters long.`
+  } else if (!hasUppercase) {
+    if (blur) errors.value.password = 'Password must contain at least one uppercase letter.'
+  } else if (!hasLowercase) {
+    if (blur) errors.value.password = 'Password must contain at least one lowercase letter.'
+  } else if (!hasNumber) {
+    if (blur) errors.value.password = 'Password must contain at least one number.'
+  } else if (!hasSpecialChar) {
+    if (blur) errors.value.password = 'Password must contain at least one special character.'
+  } else {
+    errors.value.password = null
+  }
+}
 </script>
 
-<style></style>
+<style>
+.container {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  max-width: 100%;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: white;
+  border-radius: 10px;
+}
+
+/* Class selectors */
+.form {
+  text-align: center;
+  margin-top: 50px;
+}
+.form-label {
+  font-size: 1.25rem;
+}
+
+.text-center {
+  width: 100%;
+}
+</style>
